9f90df714d6181188bf022e6cecc7200cd11299f
Malkanna <113902709+Malkanna@users.noreply.github.com>
2026-05-24 17:00:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import os
 
 # import os and use it to get the Github repository secrets
 MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PWD = os.environ.get("MY_PASSWORD")
+MY_PWD = os.environ.get("MY_PWD")
 
 today = datetime.now()
 today_tuple = (today.month, today.day)
